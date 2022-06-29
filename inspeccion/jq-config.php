6c896651436a3f82d26efde0aec66da941351433
David Ramirez <david.ramirez@auxers.com>
2022-06-29 20:30:10 +0200

    Cambio contructor llamada BBDD en el configurardo del GRID. Pero no soluciona...

--- inspeccion/jq-config.php
+++ inspeccion/jq-config.php
@@ -3,7 +3,7 @@
 //define('DB_DSN','mysql:host=localhost;port=3306;dbname=qpq363');
 //define('DB_DSN','mysql:host=localhost:3306;dbname=qpq363');
 
-define('DB_DSN','mysql:host=qpq363.abantosvertical.com:3306;dbname=qpq363');
+define('DB_DSN','mysql:host=localhost;dbname=qpq363');
 
 define('DB_USER', 'qpq363');				 // Your MySQL username
 define('DB_PASSWORD', '2013abantoS');		//	and password
